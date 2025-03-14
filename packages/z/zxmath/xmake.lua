c5bab369dc88e0b579d4f5784f8cea487a02fa50
ZXPrism <zxprism03@gmail.com>
2025-03-14 19:27:17 +0800

    update

--- packages/z/zxmath/xmake.lua
+++ packages/z/zxmath/xmake.lua
@@ -1,7 +1,8 @@
 package("zxmath")
     set_description("A math library made for my soft renderer 🌠")
     set_license("MIT")
-    set_urls("https://github.com/ZXPrism/ZXMath.git")
+
+    add_urls("https://github.com/ZXPrism/ZXMath.git")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)

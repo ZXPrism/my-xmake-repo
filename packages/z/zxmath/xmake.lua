872e6bc623fbc6b9fb2de1fc7affab102f49769a
ZXPrism <zxprism03@gmail.com>
2025-03-14 19:48:01 +0800

    update

--- packages/z/zxmath/xmake.lua
+++ packages/z/zxmath/xmake.lua
@@ -3,7 +3,7 @@ package("zxmath")
     set_license("MIT")
 
     add_urls("https://github.com/ZXPrism/ZXMath.git")
-    add_versions("latest", "26ffe9f7fb2beeff2dfa139727835f47cd3b2d07")
+    add_versions("latest", "eec22481ec378379eb1fad5a361438024f2a9cfe")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
